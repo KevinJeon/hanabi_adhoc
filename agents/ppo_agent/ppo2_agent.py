7e88be7ded4af2818c0bed941f8ae6675d2c8d0b
kevinjeon <kevinjeon119@gmail.com>
2020-11-16 00:17:09 +0900

    fix train (ing)

--- agents/ppo_agent/ppo2_agent.py
+++ agents/ppo_agent/ppo2_agent.py
@@ -33,15 +33,12 @@ class Policy(nn.Module):
   def forward(self, observations):
     raise NotImplementedError
 
-  def act(self, observations, deterministic=False):
+  def act(self, observations, legal_actions):
     v = self.critic_linear(self.critic(observations))
     h_a = self.actor(observations)
+    h_a[legal_actions!=0] = 0
     dist = self.dist(h_a)
-    if deterministic:
-      action = dist.mode()
-    else:
-      action = dist.sample()
-
+    action = dist.sample()
     action_log_probs = dist.log_probs(action)
     dist_entropy = dist.entropy().mean()
 
@@ -68,11 +65,11 @@ class DBPLAgent(object):
     for i in range(num_player):
       self.policy_modules[i] = PolicyNet(num_state,num_action,num_hidden).cuda()
       self.old_policy_modules[i] = PolicyNet(num_state, num_action, num_hidden).cuda()
-    self.transitions = [[] for _ in range(num_player)]
     self.num_player = num_player
     self.num_belief = num_belief
     self.num_action = num_action
     self.memory = RolloutStorage(batch_size=64,num_agent=num_player,obs_shape=num_state)
+    self.memory.cuda()
     if not os.path.exists(log_dir):
       os.mkdir(log_dir)
     self.writer = SummaryWriter(log_dir)
@@ -119,46 +116,50 @@ class DBPLAgent(object):
       #print('end',end,'memory_count',self.memory.add_count)
       for i in range(self.num_player):
         self.iter[i] += 1
+        advantages = self.memory.returns[i][:-1] - self.memory.value_preds[i][:-1]
+        advantages = (advantages - advantages.mean())/ (advantages.std()+1e-5)
         # train policy module
-        observations,actions,returns,old_logprobs,value_preds,dones,adv_targ\
-          = self.memory.get_batch(i)
-        if len(observations) == 0:
-          self.iter[i] -= 1
-          continue
-        advantages = returns - value_preds
-        if len(rewards) == 1:
-          pass
-        else:
-          advantages = (advantages - advantages.mean()) / (advantages.std() + 1e-5)
-        #print(len(old_obss),len(old_actions),len(old_logprobs),len(old_beliefs),len(confs.detach()))
+        avg_val_loss = 0
+        avg_act_loss = 0
+        avg_dist = 0
         for _ in range(self.k):
-          values,action_log_probs,dist_entropy = self.policy_modules[i].evaluate_actions(observations,actions)
-          ratio = tr.exp(action_log_probs-old_logprobs.detach())
-          surr_loss1 = ratio*adv_targ
-          surr_loss2 = tr.clamp(ratio,1-self.clip_param,1+self.clip_param)*adv_targ
-          action_loss = -tr.min(surr_loss1, surr_loss2).mean()
-          if self.use_clipped_value_loss:
-            value_pred_clipped = value_preds + \
-                                 (values - value_preds).clamp(-self.clip_param, self.clip_param)
-            value_losses = (values - returns).pow(2)
-            value_losses_clipped = (
-                    value_pred_clipped - returns).pow(2)
-            value_loss = 0.5 * tr.max(value_losses,
-                                         value_losses_clipped).mean()
-          else:
-            value_loss = 0.5 * (returns - values).pow(2).mean()
-          self.p_optim[i].zero_grad()
-          (value_loss * self.value_loss_coef + action_loss -
-           dist_entropy * self.entropy_coef).backward()
-          nn.utils.clip_grad_norm_(self.actor_critic.parameters(),
-                                   self.max_grad_norm)
-          self.p_optim[i].step()
-        self.writer.add_scalar('policy_loss/agent{}'.format(i), loss.mean(),self.iter[i])
+          sampler = self.memory.feed_forward_generator(advantages,self.batch_size)
+          for sample in sampler:
+            obss,acts,val_preds,rets,masks,old_probs,adv_targ = sample
+            vals,log_probs,dist_entropy,_ = self.policy_modules[i].evaluate_actions(obss,masks,acts)
+            ratio = tr.exp(log_probs-old_probs.detach())
+            surr_loss1 = ratio*adv_targ
+            surr_loss2 = tr.clamp(ratio,1-self.clip_param,1+self.clip_param)*adv_targ
+            action_loss = -tr.min(surr_loss1, surr_loss2).mean()
+            if self.use_clipped_value_loss:
+              value_pred_clipped = val_preds + \
+                                   (vals - val_preds).clamp(-self.clip_param, self.clip_param)
+              value_losses = (vals - rets).pow(2)
+              value_losses_clipped = (value_pred_clipped - rets).pow(2)
+              value_loss = 0.5 * tr.max(value_losses,value_losses_clipped).mean()
+            else:
+              value_loss = 0.5 * (rets - vals).pow(2).mean()
+            self.p_optim[i].zero_grad()
+            (value_loss * self.value_loss_coef + action_loss -
+             dist_entropy * self.entropy_coef).backward()
+            nn.utils.clip_grad_norm_(self.actor_critic.parameters(),
+                                     self.max_grad_norm)
+            self.p_optim[i].step()
+            avg_val_loss += value_loss.item()
+            avg_dist += dist_entropy.item()
+            avg_act_loss += action_loss.item()
+        avg_val_loss /= self.k*self.batch_size
+        avg_act_loss /= self.k*self.batch_size
+        avg_dist /= self.k * self.batch_size
+        self.writer.add_scalar('action_loss/agent{}'.format(i), avg_act_loss,self.iter[i])
+        self.writer.add_scalar('value_loss/agent{}'.format(i), avg_val_loss,self.iter[i])
+        self.writer.add_scalar('entropy/agent{}'.format(i), avg_dist, self.iter[i])
         #print('agent : {} policy loss : {} belief loss : {}'.format(i,loss.mean().item(),kl_loss.mean().item()))
         self.old_policy_modules[i].load_state_dict(self.policy_modules[i].state_dict())
-      self.memory.clear()
+      self.memory.after_update()
   def _select_action(self,current_player,observation,legal_actions):
     observation = tr.from_numpy(observation).float().cuda()
-    action,dist = self.old_policy_modules[current_player].act(observation,legal_actions)
+    with tr.no_grad():
+      action,dist = self.old_policy_modules[current_player].act(observation,legal_actions)
     return action,dist.log_prob(action)
 
